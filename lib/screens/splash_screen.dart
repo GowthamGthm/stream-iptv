0e0c2edfcd5bb4dd2cc7c73f459f2bdbf9c38c24
gowtham <gowtham.gthm@gmail.com>
2025-10-19 05:55:45 +0530

    changes

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -13,8 +13,8 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     // Set a timer for 3 seconds, then navigate to the HomeScreen
-    Timer(const Duration(seconds: 3), () {
-      // Navigator.pushReplacementNamed(context, '/home');
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, '/home');
     });
   }
 
@@ -28,6 +28,10 @@ class _SplashScreenState extends State<SplashScreen> {
           
           children: [
             Image.asset("assets/images/app_logo_no_bg.png", height: 300, width: 300),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
+            ),
+            SizedBox(height: 20),
             const Text(
               'Your Ultimate Streaming Companion',
               style: TextStyle(color: Colors.black54, fontSize: 20 , fontWeight: FontWeight.bold),
